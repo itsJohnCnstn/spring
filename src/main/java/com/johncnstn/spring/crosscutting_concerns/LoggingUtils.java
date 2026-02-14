27c1d216ba245c3d212181662deb4bd902f604f9
Pavel Khankevich <johncnstn@gmail.com>
2026-02-14 17:18:21 +0100

    [crosscutting_concerns] implement Aspect, Interceptor and Filter

---
+++ src/main/java/com/johncnstn/spring/crosscutting_concerns/LoggingUtils.java
@@ -0,0 +1,21 @@
+package com.johncnstn.spring.crosscutting_concerns;
+
+import org.springframework.aop.framework.AopProxyUtils;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+public abstract class LoggingUtils {
+
+    private LoggingUtils() {
+    }
+
+    public static void logObjectMetadata(Object object) {
+        Instant now = Instant.now().truncatedTo(ChronoUnit.MILLIS);
+        System.out.println("When: " + now);
+        System.out.println("Runtime class: " + object.getClass().getName());
+        System.out.println("Ultimate target class: " + AopProxyUtils.ultimateTargetClass(object).getName());
+        System.out.println("Instance: " + object);
+    }
+
+}
---
+++ src/main/java/com/johncnstn/spring/crosscutting_concerns/interceptor/LoggingInterceptor.java
@@ -0,0 +1,22 @@
+package com.johncnstn.spring.crosscutting_concerns.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import static com.johncnstn.spring.crosscutting_concerns.LoggingUtils.logObjectMetadata;
+
+@Component
+public class LoggingInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        LoggingInterceptor interceptor = this;
+        System.out.println("\n===Interceptor:===");
+        logObjectMetadata(interceptor);
+        System.out.println("===endInterceptor===\n");
+        return true;
+    }
+
+}
