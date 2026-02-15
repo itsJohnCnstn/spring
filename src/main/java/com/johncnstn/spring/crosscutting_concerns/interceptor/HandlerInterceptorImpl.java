0182d9968e50b03bf1e89b86f373e6bdbc9a0913
Pavel Khankevich <johncnstn@gmail.com>
2026-02-15 16:52:51 +0100

    [crosscutting_concerns] update HandlerInterceptor log

--- src/main/java/com/johncnstn/spring/crosscutting_concerns/interceptor/HandlerInterceptorImpl.java
+++ src/main/java/com/johncnstn/spring/crosscutting_concerns/interceptor/HandlerInterceptorImpl.java
@@ -1,38 +1,44 @@
 package com.johncnstn.spring.crosscutting_concerns.interceptor;
 
+import com.johncnstn.spring.crosscutting_concerns.enums.AdviceType;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import static com.johncnstn.spring.crosscutting_concerns.utils.LoggingUtils.endOfCustomLogBlock;
 import static com.johncnstn.spring.crosscutting_concerns.utils.LoggingUtils.logCrossCuttingConcernCallerMetadata;
+import static com.johncnstn.spring.crosscutting_concerns.utils.LoggingUtils.startOfCustomLogBlock;
 
 @Component
 public class HandlerInterceptorImpl implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String className = this.getClass().getSimpleName();
+        startOfCustomLogBlock(className, AdviceType.BEFORE);
 
         HandlerInterceptorImpl interceptor = this;
-        System.out.println("===Interceptor:===");
         logCrossCuttingConcernCallerMetadata(interceptor);
 
+        StringBuilder stringBuilder = new StringBuilder("");
         if (handler instanceof HandlerMethod handlerMethod) {
 
             // TargetController isn’t being advised by any AOP pointcut, so Spring has no reason to wrap it
             // It was a CGLib proxy when there was a pointcut pointed
-            Object beanClass = handlerMethod.getBean().getClass();
-            String simpleName = handlerMethod.getBeanType().getSimpleName();
+            String beanClassName = handlerMethod.getBean().getClass().getName();
+            String beanTypeSimpleName = handlerMethod.getBeanType().getSimpleName();
             String methodName = handlerMethod.getMethod().getName();
-            System.out.println("### Target class:");
-            System.out.println("beanClass: " + beanClass);
-            System.out.println("beanSimpleName: " + simpleName);
-            System.out.println("methodName: " + methodName);
-            System.out.println("###");
+
+            stringBuilder.append("""
+                    beanClassName: %s
+                    beanTypeSimpleName: %s
+                    methodName: %s
+                    """.formatted(beanClassName, beanTypeSimpleName, methodName));
         }
 
-        System.out.println("===endInterceptor===");
+        endOfCustomLogBlock(stringBuilder.toString());
         return true;
     }
 
